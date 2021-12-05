901afc780fe80626ba35eee218a89105e58cbd5e
Chirag Goel <61325696+chirag-goel360@users.noreply.github.com>
2021-12-05 15:07:26 +0530

    modification

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const btn = document.querySelector('button');
 
+// Set the background color and background Image of window (Screen)
 document.body.style.backgroundColor = "#2b1331";
 document.body.style.backgroundImage = "linear-gradient(315deg, #2b1331 0%, #b9abcf 74%)";
 
+// Add an EventListener of mouseover so that on mouseover the button changes its position
 btn.addEventListener('mouseover', function(){
     const height = Math.floor(Math.random() * window.innerHeight);
     const width = Math.floor(Math.random() * window.innerWidth);
@@ -10,6 +12,7 @@ btn.addEventListener('mouseover', function(){
     btn.style.top = `${height}px`; 
 });
 
+// Add an EventListener of click so that on clicking its text and background chnages
 btn.addEventListener('click', function(){
     btn.innerText = 'Got Me';
     document.body.style.backgroundColor = 'green';
